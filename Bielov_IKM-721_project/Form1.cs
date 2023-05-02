73a8cdc87e6d395469d4316875d622015004a6b8
godsyndrome <horizza53@gmail.com>
2023-05-03 01:23:43 +0300

    Finish Task number 3

--- Bielov_IKM-721_project/Form1.cs
+++ Bielov_IKM-721_project/Form1.cs
@@ -31,10 +31,10 @@ namespace Bielov_IKM_721_project
         private void Form1_Load(object sender, EventArgs e)
         {
             About A = new About(); // створення форми About
-
             A.tAbout.Start();
             A.ShowDialog(); // відображення діалогового вікна About
             MajorObject = new MajorWork();
+            MajorObject.SetTime();
             this.Mode = true;
         }
 
@@ -80,6 +80,13 @@ namespace Bielov_IKM_721_project
                 tClock.Start();
                 e.KeyChar = (char)0;
             }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            string s;
+            s = (System.DateTime.Now - MajorObject.GetTime()).ToString();
+            MessageBox.Show(s, "Час роботи програми");// Виведення часу роботи програми і повідомлення "Час роботи програми" на екран
         }
     }
 }
--- Bielov_IKM-721_project/MajorWork.cs
+++ Bielov_IKM-721_project/MajorWork.cs
@@ -9,9 +9,19 @@ namespace Surname_Group_Course_project
     {
         // Вміст робочого об'єкта
         // Поля
+        private System.DateTime TimeBegin; // час початку роботи програми
         private string Data; //вхідні дані
         private string Result; // Поле результату
                                // Методи
+        public void SetTime() // метод запису часу початку роботи програми
+        {
+            this.TimeBegin = System.DateTime.Now;
+        }
+
+        public System.DateTime GetTime() // Метод отримання часу завершення програми
+        {
+            return this.TimeBegin;
+        }
         public void Write(string D)// метод запису даних в об'єкт.
         {
             this.Data = D;
