7e2e1887266d7a6ff49c7da04cbe1682febbfb59
godsyndrome <horizza53@gmail.com>
2023-05-03 02:15:09 +0300

    Finish Task number 4

--- Bielov_IKM-721_project/MajorWork.cs
+++ Bielov_IKM-721_project/MajorWork.cs
@@ -12,7 +12,17 @@ namespace Surname_Group_Course_project
         private System.DateTime TimeBegin; // час початку роботи програми
         private string Data; //вхідні дані
         private string Result; // Поле результату
-                               // Методи
+        private string SaveFileName;// ім’я файлу для запису
+        private string OpenFileName;// ім’я файлу для читання
+        public void WriteSaveFileName(string S)// метод запису даних в об'єкт
+        {
+            this.SaveFileName = S;// запам'ятати ім’я файлу для запису
+        }
+        public void WriteOpenFileName(string S)
+        {
+            this.OpenFileName = S;// запам'ятати ім’я файлу для відкриття
+        }
+        // Методи
         public void SetTime() // метод запису часу початку роботи програми
         {
             this.TimeBegin = System.DateTime.Now;
