61da235b7f86e784c9b93e424e6f27146d55925b
godsyndrome <horizza53@gmail.com>
2023-05-03 00:53:11 +0300

    Finish Task number 1

--- Bielov_IKM-721_project/Form1.Designer.cs
+++ Bielov_IKM-721_project/Form1.Designer.cs
@@ -29,12 +29,68 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bStart = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tClock = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(386, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(371, 288);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(75, 23);
+            this.bStart.TabIndex = 1;
+            this.bStart.Text = "Пуск";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Enabled = false;
+            this.tbInput.Location = new System.Drawing.Point(316, 217);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(189, 20);
+            this.tbInput.TabIndex = 2;
+            this.tbInput.TextChanged += new System.EventHandler(this.tbInput_TextChanged);
+            this.tbInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbInput_KeyPress);
+            // 
+            // tClock
+            // 
+            this.tClock.Interval = 25000;
+            this.tClock.Tick += new System.EventHandler(this.tClock_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1478, 774);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Timer tClock;
     }
 }
 
--- Bielov_IKM-721_project/Form1.cs
+++ Bielov_IKM-721_project/Form1.cs
@@ -12,9 +12,63 @@ namespace Bielov_IKM_721_project
 {
     public partial class Form1 : Form
     {
+        private bool Mode; // Режим дозволу / заборони введення даних
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void tClock_Tick(object sender, EventArgs e)
+        {
+            tClock.Stop();
+            MessageBox.Show("Минуло 25 секунд", "Увага");// Виведення повідомлення "Минуло 25 секунд" на екран
+            tClock.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Mode = true;
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if (Mode)
+            {
+                tbInput.Enabled = true;// Режим дозволу введення 
+                tbInput.Focus();
+                tClock.Start();
+                bStart.Text = "Стоп"; // зміна тексту на кнопці на "Стоп"
+                this.Mode = false;
+            }
+            else
+            {
+                tbInput.Enabled = false;// Режим заборони введення
+                tClock.Stop();
+                bStart.Text = "Пуск";// зміна тексту на кнопці на "Пуск"
+                this.Mode = true;
+            }
+        }
+
+        private void tbInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            tClock.Stop();
+            tClock.Start();
+            if ((e.KeyChar >= '0') & (e.KeyChar <= '9') | (e.KeyChar == (char)8))
+            {
+                return;
+            }
+            else
+            {
+                tClock.Stop();
+                MessageBox.Show("Неправильний символ", "Помилка");
+                tClock.Start();
+                e.KeyChar = (char)0;
+            }
         }
     }
 }
